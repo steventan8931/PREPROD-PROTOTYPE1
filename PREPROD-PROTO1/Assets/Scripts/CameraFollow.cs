6d0b259fc2b511be90c1dd3e6afe42dd3f576b6b
Steven Tan <tan2001steven@hotmail.com>
2022-02-22 19:30:47 +1300

    Project

---
+++ PREPROD-PROTO1/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public float m_FollowSpeed = 5.0f;
+    public Transform m_CameraOffset;
+
+    private void Update()
+    {
+        if (transform.position != m_CameraOffset.position)
+        {
+            transform.position = Vector3.Lerp(transform.position, m_CameraOffset.position, Time.deltaTime * m_FollowSpeed);
+        }
+    }
+}
---
+++ PREPROD-PROTO1/Assets/Scripts/CharacterMotor.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMotor : MonoBehaviour
+{
+    public float m_MoveSpeed = 5.0f;
+
+    public Rigidbody m_Rigid;
+
+    private Vector3 m_Movement;
+
+    private void Start()
+    {
+        m_Rigid = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        m_Movement.x = Input.GetAxisRaw("Horizontal");
+        m_Movement.z = Input.GetAxisRaw("Vertical");
+
+        m_Rigid.MovePosition(m_Rigid.position + m_Movement * m_MoveSpeed * Time.deltaTime);
+    }
+}
