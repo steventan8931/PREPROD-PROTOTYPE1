0d7b0648e85fe02d0583ecd3fa3a57ceca67f888
Steven Tan <tan2001steven@hotmail.com>
2022-02-22 23:20:24 +1300

    Character Direction + Attacking Object

---
+++ Assets/Scripts/Interactable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactable : MonoBehaviour
+{
+    [Header("Interactable Components")]
+    public int m_Health = 10;
+    public GameObject m_DropPrefab;
+
+    public void TakeDamage(int _Damage)
+    {
+        if (m_Health > 0)
+        {
+            m_Health -= _Damage;
+        }
+    }
+}
