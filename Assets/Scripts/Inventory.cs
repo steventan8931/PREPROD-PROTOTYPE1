48c550f91d223ec0ecbad751f8fad23e8419a744
Steven Tan <tan2001steven@hotmail.com>
2022-02-23 13:02:05 +1300

    Inventory Setup

--- Assets/Scripts/Inventory.cs
+++ Assets/Scripts/Inventory.cs
@@ -9,10 +9,33 @@ public class Inventory : MonoBehaviour
     public int m_RockCount = 0;
 
     public Text m_PressGText;
+    public GameObject m_Inventory;
+    public bool m_InventoryOpen = false;
 
     private void Start()
     {
         m_PressGText.enabled = false;
+        UpdateInventory();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I))
+        {
+            m_InventoryOpen = !m_InventoryOpen;
+            UpdateInventory();
+        }
+    }
+
+    private void UpdateInventory()
+    {
+        if (m_InventoryOpen)
+        {
+            m_Inventory.SetActive(true);
+        }
+        else
+        {
+            m_Inventory.SetActive(false);
+        }
+    }
 }
