d6fcab86cffb9adbcfbebb33ded6ad2285bd06e2
peter <hguanqi7@gmail.com>
2022-02-28 13:52:31 +0800

    Working on quest system

---
+++ Assets/Scripts/QuestGoal.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class QuestGoal 
+{
+    public GoalType goalType;
+    public int requiredAmount;
+    public int currentAmount;
+
+    public bool IsReached()
+    {
+        return (currentAmount >= requiredAmount);
+    }
+
+    public void WoodGathered()
+    {
+        if(goalType == GoalType.GatheringWood)
+           currentAmount++;
+    }
+
+    public void RockGathered()
+    {
+        if (goalType == GoalType.GatheringRock)
+            currentAmount++;
+    }
+
+    public void EnemyKilled()
+    {
+        if (goalType == GoalType.Kill)
+            currentAmount++;
+    }
+}
+
+public enum GoalType
+{
+    GatheringWood,
+    GatheringRock,
+    Kill
+}
