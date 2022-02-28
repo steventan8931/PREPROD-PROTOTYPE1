73c6f832428ec257bf871f0e598a47a0de40efd5
peter <hguanqi7@gmail.com>
2022-02-28 11:04:03 +0800

    Import TMP, add quest window

---
+++ Assets/QuestGiver.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+public class QuestGiver : MonoBehaviour
+{
+    public Quest quest;
+
+    public Inventory playerInventory;
+
+    public GameObject questWindow;
+    public TextMeshProUGUI titleText;
+    public TextMeshProUGUI descriptionText;
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI rockText;
+    private void Awake()
+    {
+        playerInventory = GameObject.FindGameObjectWithTag("Player").GetComponent<Inventory>();
+    }
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            openQuestWindow();
+        }
+    }
+    public void openQuestWindow()
+    {
+        questWindow.SetActive(true);
+        titleText.text = quest.title;
+        descriptionText.text = quest.description;
+        woodText.text = quest.woodReward.ToString();
+        rockText.text = quest.rockReward.ToString();
+    }
+}
