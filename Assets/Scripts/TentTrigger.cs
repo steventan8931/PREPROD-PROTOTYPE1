511ec8eabe712add33ee550455e016c2e04e3fdd
Steven Tan <tan2001steven@hotmail.com>
2022-03-11 12:46:39 +1300

    End Game Scene + Fade

--- Assets/Scripts/TentTrigger.cs
+++ Assets/Scripts/TentTrigger.cs
@@ -2,16 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 public class TentTrigger : MonoBehaviour
 {
     public Animator m_Animation;
     private CharacterMotor m_Player;
     public bool m_IsColliding = false;
     public bool m_TentOpen = false;
+    public GameObject m_EndUI;
 
+    public float m_Timer = 0.0f;
+    public float m_FadeTime = 2.0f;
+    private Color cachecolor;
     private void Start()
     {
         //m_Animation = GetComponent<Animator>();
+        cachecolor = m_EndUI.transform.GetChild(0).GetComponent<Image>().color;
+        m_EndUI.transform.GetChild(0).GetComponent<Image>().color = new Color(cachecolor.r,cachecolor.g,cachecolor.b, 0.0f);
     }
 
     private void OnTriggerStay(Collider other)
@@ -39,16 +46,24 @@ public class TentTrigger : MonoBehaviour
             Debug.Log("in tent");
             if (Input.GetKeyDown(KeyCode.F))
             {
+                m_EndUI.SetActive(true);
                 m_Player.m_Animation.ResetTrigger("Attacking");
                 m_Player.m_Animation.SetTrigger("Attacking");
-                m_TentOpen = !m_TentOpen;
+                m_TentOpen = true;
             }
         }
 
         if (m_TentOpen)
         {
             m_Animation.SetBool("open", true);
-            SceneManager.LoadScene(2);
+            m_Timer += Time.deltaTime;
+
+            m_EndUI.transform.GetChild(0).GetComponent<Image>().color = new Color(cachecolor.r, cachecolor.g, cachecolor.b, m_Timer / m_FadeTime);
+            if (m_Timer >= m_FadeTime)
+            {
+                SceneManager.LoadScene(2);
+            }
+
         }
         else
         {
